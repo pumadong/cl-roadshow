95f45c84e5b451caffcbdfeaf7df95412a66828d
董永进 <dongyongjin@meituan.com>
2015-06-16 11:52:15 +0800

    时间戳和时间字符串互转：增加毫秒的转换

--- roadshow-app/src/main/java/com/cl/roadshow/general/TimeStampAndDateDemo.java
+++ roadshow-app/src/main/java/com/cl/roadshow/general/TimeStampAndDateDemo.java
@@ -15,27 +15,48 @@ public class TimeStampAndDateDemo {
 
         String date = timeStamp2Date(timeStampSource, "yyyy-MM-dd HH:mm:ss");
         System.out.println("date=" + date);
+        
+        String dateMilli = timeStamp2DateMilli(timeStampSource, "yyyy-MM-dd HH:mm:ss.FFF");
+        System.out.println("dateMilli=" + dateMilli);
 
         String timeStampTarget = date2TimeStamp(date, "yyyy-MM-dd HH:mm:ss");
         System.out.println("timeStampTarget=" + timeStampTarget);
     }
 
     /**
-     * 时间戳转换成日期格式字符串
+     * 时间戳转换成到秒日期格式字符串
      * 
-     * @param seconds
+     * @param milliSeconds
      *            精确到秒的字符串
      * @param formatStr
      * @return
      */
-    public static String timeStamp2Date(String seconds, String format) {
-        if (seconds == null || seconds.isEmpty() || seconds.equals("null")) {
+    public static String timeStamp2Date(String milliSeconds, String format) {
+        if (milliSeconds == null || milliSeconds.isEmpty() || milliSeconds.equals("null")) {
             return "";
         }
         if (format == null || format.isEmpty())
             format = "yyyy-MM-dd HH:mm:ss";
         SimpleDateFormat sdf = new SimpleDateFormat(format);
-        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+        return sdf.format(new Date(Long.valueOf(milliSeconds)));
+    }
+    
+    /**
+     * 时间戳转换成带毫秒的日期格式字符串
+     * 
+     * @param milliSeconds
+     *            精确到毫秒的字符串
+     * @param formatStr
+     * @return
+     */
+    public static String timeStamp2DateMilli(String milliSeconds, String format) {
+        if (milliSeconds == null || milliSeconds.isEmpty() || milliSeconds.equals("null")) {
+            return "";
+        }
+        if (format == null || format.isEmpty())
+            format = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(milliSeconds)));
     }
 
     /**
@@ -50,7 +71,7 @@ public class TimeStampAndDateDemo {
     public static String date2TimeStamp(String date_str, String format) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat(format);
-            return String.valueOf(sdf.parse(date_str).getTime() / 1000);
+            return String.valueOf(sdf.parse(date_str).getTime());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -58,13 +79,13 @@ public class TimeStampAndDateDemo {
     }
 
     /**
-     * 取得当前时间戳（精确到秒）
+     * 取得当前时间戳（精确到毫秒）
      * 
      * @return
      */
     public static String timeStamp() {
         long time = System.currentTimeMillis();
-        String t = String.valueOf(time / 1000);
+        String t = String.valueOf(time);
         return t;
     }
 

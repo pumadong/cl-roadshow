4af9059a789998fe2b5962a39efa8c64f2abd587
董永进 <dongyongjin@meituan.com>
2015-06-24 12:02:01 +0800

    时间戳代码优化

--- roadshow-app/src/main/java/com/cl/roadshow/general/TimeStampAndDateDemo.java
+++ roadshow-app/src/main/java/com/cl/roadshow/general/TimeStampAndDateDemo.java
@@ -10,53 +10,32 @@ import java.util.Date;
 public class TimeStampAndDateDemo {
 
     public static void main(String[] args) {
-        String timeStampSource = timeStamp();
+        Long timeStampSource = timeStamp();
         System.out.println("timeStampFrom=" + timeStampSource);
 
         String date = timeStamp2Date(timeStampSource, "yyyy-MM-dd HH:mm:ss");
         System.out.println("date=" + date);
         
-        String dateMilli = timeStamp2DateMilli(timeStampSource, "yyyy-MM-dd HH:mm:ss.FFF");
+        String dateMilli = timeStamp2Date(timeStampSource, "yyyy-MM-dd HH:mm:ss.FFF");
         System.out.println("dateMilli=" + dateMilli);
 
-        String timeStampTarget = date2TimeStamp(date, "yyyy-MM-dd HH:mm:ss");
+        Long timeStampTarget = date2TimeStamp(dateMilli, "yyyy-MM-dd HH:mm:ss.FFF");
         System.out.println("timeStampTarget=" + timeStampTarget);
     }
 
     /**
-     * 时间戳转换成到秒日期格式字符串
+     * 时间戳转换成到日期格式字符串
      * 
      * @param milliSeconds
      *            精确到毫秒的字符串
      * @param formatStr
      * @return
      */
-    public static String timeStamp2Date(String milliSeconds, String format) {
-        if (milliSeconds == null || milliSeconds.isEmpty() || milliSeconds.equals("null")) {
-            return "";
-        }
+    public static String timeStamp2Date(long milliSeconds, String format) {
         if (format == null || format.isEmpty())
             format = "yyyy-MM-dd HH:mm:ss";
         SimpleDateFormat sdf = new SimpleDateFormat(format);
-        return sdf.format(new Date(Long.valueOf(milliSeconds)));
-    }
-    
-    /**
-     * 时间戳转换成带毫秒的日期格式字符串
-     * 
-     * @param milliSeconds
-     *            精确到毫秒的字符串
-     * @param formatStr
-     * @return
-     */
-    public static String timeStamp2DateMilli(String milliSeconds, String format) {
-        if (milliSeconds == null || milliSeconds.isEmpty() || milliSeconds.equals("null")) {
-            return "";
-        }
-        if (format == null || format.isEmpty())
-            format = "yyyy-MM-dd HH:mm:ss";
-        SimpleDateFormat sdf = new SimpleDateFormat(format);
-        return sdf.format(new Date(Long.valueOf(milliSeconds)));
+        return sdf.format(new Date(milliSeconds));
     }
 
     /**
@@ -68,14 +47,16 @@ public class TimeStampAndDateDemo {
      *            如：yyyy-MM-dd HH:mm:ss
      * @return
      */
-    public static String date2TimeStamp(String date_str, String format) {
+    public static Long date2TimeStamp(String date_str, String format) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat(format);
-            return String.valueOf(sdf.parse(date_str).getTime());
+            //TODO：这里没有毫秒了，有空看看
+            Date d = sdf.parse(date_str);
+            return d.getTime();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "";
+        return 0L;
     }
 
     /**
@@ -83,10 +64,9 @@ public class TimeStampAndDateDemo {
      * 
      * @return
      */
-    public static String timeStamp() {
+    public static Long timeStamp() {
         long time = System.currentTimeMillis();
-        String t = String.valueOf(time);
-        return t;
+        return time;
     }
 
 }

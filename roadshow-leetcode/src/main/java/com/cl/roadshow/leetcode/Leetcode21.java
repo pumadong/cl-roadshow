47aa89e5af617ea00155887dbc8c22945c56a3f7
董永进 <dongyongjin@meituan.com>
2016-07-08 13:39:13 +0800

    处理有交点的链表，这种情况如果不处理，会有死循环

--- roadshow-leetcode/src/main/java/com/cl/roadshow/leetcode/Leetcode21.java
+++ roadshow-leetcode/src/main/java/com/cl/roadshow/leetcode/Leetcode21.java
@@ -12,14 +12,23 @@ public class Leetcode21 {
 		Leetcode21 lc = new Leetcode21();
 		Solution solution = lc.new Solution();
 		System.out.println(solution.mergeTwoLists(ListNode.General.getHeadA(), ListNode.General.getHeadB()));
+		System.out.println(solution.mergeTwoLists(ListNode.Intersection.getHeadA(), ListNode.Intersection.getHeadB()));
 	}
 	
 	public class Solution {
-		// 两个链表有交点时，代码有问题，需要优化
 	    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 	        ListNode res = new ListNode(0);
 	        ListNode head = res;
 	        while(l1!=null || l2!=null) {
+	        	// 处理有交点的链表，这种情况如果不处理，会有死循环
+            	if(l1 != null && l1.next == l2) {
+            		res.next = l1;
+            		break;
+            	}
+            	if(l2 != null & l2.next == l1) {
+            		res.next = l2;
+            		break;
+            	}
 	            if(l1!=null && l2!=null) {
 	                if(l1.val > l2.val) {
 	                    res.next = l2;

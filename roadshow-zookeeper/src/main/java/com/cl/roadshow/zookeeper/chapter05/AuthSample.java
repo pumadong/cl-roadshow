737cade61f78cbc29e7b87a74ec9cf534b078f60
董永进 <dongyongjin@meituan.com>
2016-09-27 09:04:39 +0800

    更新&存在&ACL操作演示

---
+++ roadshow-zookeeper/src/main/java/com/cl/roadshow/zookeeper/chapter05/AuthSample.java
@@ -0,0 +1,17 @@
+package com.cl.roadshow.zookeeper.chapter05;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+//使用含权限信息的ZooKeeper会话创建数据节点
+public class AuthSample {
+
+    final static String PATH = "/zk-book-auth_test";
+    public static void main(String[] args) throws Exception {
+
+        ZooKeeper zookeeper = new ZooKeeper("domain1.book.zookeeper:2181",50000,null);
+        zookeeper.addAuthInfo("digest", "foo:true".getBytes());
+        zookeeper.create( PATH, "init".getBytes(), Ids.CREATOR_ALL_ACL, CreateMode.EPHEMERAL );
+        Thread.sleep( Integer.MAX_VALUE );
+    }
+}
---
+++ roadshow-zookeeper/src/main/java/com/cl/roadshow/zookeeper/chapter05/AuthSample_Delete.java
@@ -0,0 +1,34 @@
+package com.cl.roadshow.zookeeper.chapter05;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+//删除节点的权限控制
+public class AuthSample_Delete {
+
+    final static String PATH  = "/zk-book-auth_test";
+    final static String PATH2 = "/zk-book-auth_test/child";
+    public static void main(String[] args) throws Exception {
+
+        ZooKeeper zookeeper1 = new ZooKeeper("domain1.book.zookeeper:2181",5000,null);
+        zookeeper1.addAuthInfo("digest", "foo:true".getBytes());
+        zookeeper1.create( PATH, "init".getBytes(), Ids.CREATOR_ALL_ACL, CreateMode.PERSISTENT );
+        zookeeper1.create( PATH2, "init".getBytes(), Ids.CREATOR_ALL_ACL, CreateMode.EPHEMERAL );
+        
+        try {
+			ZooKeeper zookeeper2 = new ZooKeeper("domain1.book.zookeeper:2181",50000,null);
+			zookeeper2.delete( PATH2, -1 );
+		} catch ( Exception e ) {
+			System.out.println( "删除节点失败: " + e.getMessage() );
+		}
+        
+        ZooKeeper zookeeper3 = new ZooKeeper("domain1.book.zookeeper:2181",50000,null);
+        zookeeper3.addAuthInfo("digest", "foo:true".getBytes());
+		zookeeper3.delete( PATH2, -1 );
+        System.out.println( "成功删除节点：" + PATH2 );
+        
+        ZooKeeper zookeeper4 = new ZooKeeper("domain1.book.zookeeper:2181",50000,null);
+		zookeeper4.delete( PATH, -1 );
+        System.out.println( "成功删除节点：" + PATH );
+    }
+}
